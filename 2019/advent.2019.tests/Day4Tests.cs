c8c6e7fd636dd9c5c805e9c06ac6e6ef60adf6bb
zmj <zach.jeyakaran@gmail.com>
2019-12-04 13:10:41 -0700

    day 4 problem 1

---
+++ 2019/advent.2019.tests/Day4Tests.cs
@@ -0,0 +1,73 @@
+﻿using advent._2019._4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace advent._2019.tests
+{
+    public class Day4Tests
+    {
+        [Theory]
+        [InlineData(138307, new[] { 1, 3, 8, 3, 0, 7 })]
+        [InlineData(654504, new[] { 6, 5, 4, 5, 0, 4 })]
+        public void Base10Digits(int value, int[] digits)
+        {
+            var x = PasswordFilter.Base10Digits(value);
+            Assert.Equal(digits.Length, x.Length);
+            for (int i = 0; i < x.Length; i++)
+            {
+                Assert.Equal(digits[i], x[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 3, 8, 3, 0, 7 }, new[] { 1, 3, 8, 3, 0, 8 })]
+        [InlineData(new[] { 1, 9, 9, 9, 9, 9 }, new[] { 2, 0, 0, 0, 0, 0 })]
+        public void Increment(int[] oldDigits, int[] newDigits)
+        {
+            var x = PasswordFilter.Increment(oldDigits.AsSpan());
+            Assert.Equal(newDigits.Length, x.Length);
+            for (int i = 0; i < x.Length; i++)
+            {
+                Assert.Equal(newDigits[i], x[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData(223450, false)]
+        [InlineData(111111, true)]
+        public void NoDecrease(int value, bool ok)
+        {
+            var digits = PasswordFilter.Base10Digits(value);
+            Assert.Equal(ok, PasswordFilter.NoDecrease(digits));
+        }
+
+        [Theory]
+        [InlineData(123789, false)]
+        [InlineData(111111, true)]
+        public void AtLeastOneRepeat(int value, bool ok)
+        {
+            var digits = PasswordFilter.Base10Digits(value);
+            Assert.Equal(ok, PasswordFilter.AtLeastOneRepeat(digits));
+        }
+
+        [Theory]
+        [InlineData(111111, true)]
+        [InlineData(223450, false)]
+        [InlineData(123789, false)]
+        public void MeetsAll(int value, bool ok)
+        {
+            var digits = PasswordFilter.Base10Digits(value);
+            Assert.Equal(ok, new PasswordFilter().MeetsAll(digits));
+        }
+
+        [Theory]
+        [InlineData(138307, 654504, 1855)]
+        public void Day_4_1(int min, int max, int answer)
+        {
+            int x = new PasswordFilter().CountValidPasswords(min, max);
+            Assert.Equal(answer, x);
+        }
+    }
+}
---
+++ 2019/advent.2019/4/PasswordFilter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advent._2019._4
+{
+    public readonly struct PasswordFilter
+    {
+        public bool MeetsAll(ReadOnlySpan<int> digits)
+        {
+            return SixDigits(digits) &&
+                AtLeastOneRepeat(digits) &&
+                NoDecrease(digits);
+        }
+
+        public int CountValidPasswords(int min, int max)
+        {
+            var digits = Base10Digits(min).AsSpan();
+            var maxDigits = Base10Digits(max).AsSpan();
+            int count = 0;
+            do
+            {
+                if (MeetsAll(digits)) { count++; }
+                digits = Increment(digits);
+            } while (!digits.SequenceEqual(maxDigits));
+            return count;
+        }
+
+        public static bool SixDigits(ReadOnlySpan<int> digits) =>
+            digits.Length == 6;
+
+        public static bool AtLeastOneRepeat(ReadOnlySpan<int> digits)
+        {
+            for (int i = 0; i<digits.Length-1; i++)
+            {
+                if (digits[i] == digits[i+1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool NoDecrease(ReadOnlySpan<int> digits)
+        {
+            for (int i= 0; i < digits.Length - 1; i++)
+            {
+                if (digits[i] > digits[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int[] Base10Digits(int value)
+        {
+            if (value < 100000 || value > 999999) { throw new ArgumentOutOfRangeException(); };
+            int[] digits = new int[6];
+            for (int i=0; i<6; i++)
+            {
+                int base10 = 1;
+                for (int b = 5; b > i; b--) { base10 *= 10; }
+                int digit = value / base10;
+                digits[i] = digit;
+                value -= digit * base10;
+            }
+            return digits;
+        }
+
+        public static Span<int> Increment(Span<int> digits)
+        {
+            for (int i = digits.Length-1; i>=0; i--)
+            {
+                var d = ++digits[i];
+                if (d != 10) { break; }
+                digits[i] = 0;
+            }
+            return digits;
+        }
+    }
+}
