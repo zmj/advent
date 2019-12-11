e9447dd779844453dacbc812c84a7900a0ac005d
zmj <zach.jeyakaran@gmail.com>
2019-12-11 07:41:33 -0700

    day 11 problem 1

---
+++ 2019/advent.2019.tests/Day11Tests.cs
@@ -0,0 +1,22 @@
+﻿using advent._2019._11;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace advent._2019.tests
+{
+    public class Day11Tests
+    {
+        [Theory]
+        [InlineData("input_11_1", 2054)]
+        public async Task Day_11_1(string inputFile, int answer)
+        {
+            var lines = LineReader.Open(inputFile);
+            var painter = await HullPainter.Create(lines);
+            await painter.RunProgram();
+            Assert.Equal(answer, painter.PaintedPanels);
+        }
+    }
+}
---
+++ 2019/advent.2019/11/HullPainter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace advent._2019._11
+{
+    public class HullPainter
+    {
+        private readonly _2.IntComputer _computer;
+        private readonly long[] _program;
+        private readonly ChannelReader<long> _instructions;
+        private readonly ChannelWriter<long> _sensor;
+
+        private readonly HashSet<(int, int)> _whitePanels = new HashSet<(int, int)>();
+        private readonly HashSet<(int, int)> _paintedPanels = new HashSet<(int, int)>();
+        private int _x;
+        private int _y;
+        private Direction _dir;
+
+        public HullPainter(long[] program)
+        {
+            _program = program;
+            var input = Channel.CreateUnbounded<long>();
+            var output = Channel.CreateUnbounded<long>();
+            _computer = new _2.IntComputer(input.Reader, output.Writer);
+            _sensor = input.Writer;
+            _instructions = output.Reader;
+        }
+
+        public async Task RunProgram()
+        {
+            await Task.WhenAll(
+                _computer.RunProgram(_program).AsTask(),
+                PaintUntilDone().AsTask());
+        }
+
+        private async ValueTask PaintUntilDone()
+        {
+            while (await PaintOnce()) { }
+        }
+
+        public async ValueTask<bool> PaintOnce()
+        {
+            var color = _whitePanels.Contains((_x, _y)) ? 1 : 0;
+            await _sensor.WriteAsync(color);
+            if (!(await _instructions.WaitToReadAsync())) { return false; }
+            var newColor = await _instructions.ReadAsync();
+            PaintSquare(newColor);
+            var newDirection = await _instructions.ReadAsync();
+            Turn(newDirection);
+            MoveForward();
+            return true;
+        }
+
+        public void PaintSquare(long color)
+        {
+            switch (color)
+            {
+                case 0:
+                    _whitePanels.Remove((_x, _y));
+                    break;
+                case 1:
+                    _whitePanels.Add((_x, _y));
+                    break;
+                default:
+                    throw new ArgumentException($"paint instruction: {color}");
+            }
+            _paintedPanels.Add((_x, _y));
+        }
+
+        public void Turn(long direction)
+        {
+            var incr = direction switch
+            {
+                0 => -1,
+                1 => 1,
+                _ => throw new ArgumentException($"turn instruction: {direction}")
+            };
+            var newDir = (int)_dir + incr;
+            if (newDir < 0) { newDir += 4; }
+            _dir = (Direction)(newDir % 4);
+        }
+
+        public void MoveForward()
+        {
+            var (dx, dy) = _dir switch
+            {
+                Direction.Up => (0, -1),
+                Direction.Left => (1, 0),
+                Direction.Down => (0, 1),
+                Direction.Right => (-1, 0),
+                _ => throw new ArgumentException($"move direction: {_dir}")
+            };
+            _x += dx;
+            _y += dy;
+        }
+
+        public int PaintedPanels => _paintedPanels.Count;
+
+        public static async ValueTask<HullPainter> Create(
+            IAsyncEnumerable<string> lines)
+        {
+            var line = await lines.Single();
+            var program = _2.IntComputer.ParseLine(line);
+            return new HullPainter(program);
+        }
+    }
+
+    public enum Direction { Up, Right, Down, Left }
+}
